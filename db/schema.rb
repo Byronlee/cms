2adb34c47854549ae7f405ab7377be7bc0fa8fa8
Chad Liu <liuwenmin@36kr.com>
2015-01-19 16:26:43 +0800

    去除email和phone的默认为空字符串约束

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150117094603) do
+ActiveRecord::Schema.define(version: 20150119073941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,8 +51,8 @@ ActiveRecord::Schema.define(version: 20150117094603) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "phone",                               null: false
+    t.string   "email"
+    t.string   "phone"
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
