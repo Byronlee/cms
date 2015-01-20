0930bc1b9d6f6ec03fc427ce83916bb13bf2e20a
Byronlee <lbyronlee@gmail.com>
2015-01-20 13:16:13 +0800

    重构Next collections数据获取方式，从Redis中读取

--- spec/controllers/components/next_controller_spec.rb
+++ spec/controllers/components/next_controller_spec.rb
@@ -6,6 +6,7 @@ describe Components::NextController do
     it "return http success and return the next collections" do
       get 'collections'
       response.should be_success
+      expect(response.header["Content-Type"]).to eq("application/json; charset=utf-8")
     end
   end
 
