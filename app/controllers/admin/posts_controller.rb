1dcc56305936cf072416d78ef22501d6923094e1
Chad Liu <liuwenmin@36kr.com>
2015-01-20 16:56:37 +0800

    为form添加disable和专栏选择

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,11 +1,11 @@
 class Admin::PostsController < Admin::BaseController
 
   def index
-  	@posts = Post.all
+  	@posts = Post.includes(:author, :column).all
   end
 
   def update
-  	@post.save
+    @post.update(post_params)
   	respond_with @post, location: admin_posts_path
   end
 
@@ -23,7 +23,7 @@ class Admin::PostsController < Admin::BaseController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, :slug, :summary, :title_link) if params[:post]
+    params.require(:post).permit(:column_id, :title, :content, :slug, :summary, :title_link) if params[:post]
   end
 
 end
