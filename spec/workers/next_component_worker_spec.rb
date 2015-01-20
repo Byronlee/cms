6338ad793d8d54a53a97e83eaef78343f1a73792
Byronlee <lbyronlee@gmail.com>
2015-01-20 13:17:40 +0800

    将Next API的读取放入到后台队列，并存入Redis

---
+++ spec/workers/next_component_worker_spec.rb
@@ -0,0 +1,18 @@
+require "spec_helper"
+
+describe NextComponentWorker, sidekiq: :fake do
+  it { should be_retryable true }
+  it { should be_retryable true }
+  it { should be_processed_in :krx2015 }
+
+  context ".perform" do 
+  	let(:next_redis_db) { Redis::HashKey.new('next') }
+
+  	it "enqueues access next collections job" do 
+  	  subject.perform
+  	  expect( next_redis_db.ttl ).to  be > 0
+  	  expect { next_redis_db["collections"] }.to_not be_nil
+  	end
+  end
+
+end
