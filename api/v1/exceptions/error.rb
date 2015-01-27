c3b656c02d03dfce8af81c45f4da4bc41926ffab
tian-xiaobo <peterwillcn@msn.com>
2015-01-27 13:17:21 +0800

    重构

--- api/v1/exceptions/error.rb
+++ api/v1/exceptions/error.rb
@@ -1,5 +1,17 @@
 module V1
   module Exceptions
+    module ExceptionHandlers
+
+      def self.included(base)
+        base.instance_eval do
+          rescue_from Grape::Exceptions::ValidationErrors do |e|
+            Rack::Response.new({
+              error: { code: 1001, message: e.message }
+            }.to_json, e.status)
+          end
+        end
+      end
+    end
 
     # code: api error code defined by 36kr by xiaobo, errors originated from
     # subclasses of Error have code start from 2000.
