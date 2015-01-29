0ad46cb0f14849e004a93f07518dabdd42384cf5
Byronlee <lbyronlee@gmail.com>
2015-01-29 16:12:10 +0800

    API测试Demo

--- spec/api/posts_spec.rb
+++ spec/api/posts_spec.rb
@@ -1,79 +1,33 @@
 require "spec_helper"
 
-describe API::API, "posts", :type => :request do
+describe API::API do
   include ApiHelpers
   include V1::Helpers
 
-  let(:post) { create(:post) }
-
-  describe "GET /api/v1/posts/index.json" do
-    it "should return an array of posts" do
-      get "/api/v1/posts/index.json"
-      response.status.should == 200
-      json_response.should be_an Array
+  describe "GET /api/v1/posts" do
+    it "returns an empty array of statuses" do
+      post = create :post
+      get "/api/v1/posts/index.json", state: post.state
+      expect(response.status).to eq(200)
+      #expect(response.body).to eq [post.to_json]
     end
   end
 
-  describe "GET /api/v1/posts/:id/page.json" do
-    it "should return an down array of posts" do
-      get "/api/v1/posts/1/page.json?state=draft"
-      response.status.should == 404
+  describe "POST /api/v1/posts/new" do
+    it "returns an a new post" do
+      post "/api/v1/posts/new", attributes_for :post
+      expect(response.status).to eq(200)
+      #expect(response.body).to eq [post.to_json]
     end
   end
 
   describe "GET /api/v1/posts/:id" do
-    it "should return a post by id" do
-      get "/api/v1/posts/#{post.id}.json"
-      response.status.should == 200
-      json_response['title'].should == post.title
+    it "returns an a  post" do
+      post = create :post
+      get "/api/v1/posts/"+post.id.to_s
+      expect(response.status).to eq(200)
+      #expect(response.body).to eq [post.to_json]
     end
   end
-
-  describe "PUT 'update' /api/v1/posts/:id" do
-    it "should return a post by id" do
-      put "/api/v1/posts/#{post.id}.json?title=patch"
-      response.status.should == 200
-      json_response['title'].should == 'patch'
-    end
-  end
-
-  describe "DELETE /api/v1/posts/:id" do
-    it "should return a post by id" do
-      delete "/api/v1/posts/#{post.id}.json"
-      response.status.should == 200
-    end
-  end
-
-  describe 'POST /api/v1/posts' do
-    let(:path) { '/api/v1/posts' }
-    let(:attributes) { FactoryGirl.attributes_for(:post) }
-
-    describe 'validations' do
-      context 'when title not exist in params' do
-        let(:attributes) { FactoryGirl.attributes_for(:post).except(:title) }
-        it  do
-          binding.pry
-          post path, :summary=>"summary", :content=>"content", :title_link=>"title_link", :must_read=>"true", :state=>"draft"
-          
-          expect(response).not_to be_success
-          expect(response.status).to eq(400)
-        end
-      end
-    end
-
-    it 'responds successfully' do
-      post path, attributes
-
-      expect(response).to be_success
-      expect(response.status).to eq(201)
-    end
-
-    it 'creates a new Post' do
-      expect {
-        post path, attributes
-      }.to change(Post, :count).by(1)
-    end
-  end
-
 
 end
