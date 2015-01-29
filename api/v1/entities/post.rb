06570f865a078273bc9fa763e907047991d211ab
tian-xiaobo <peterwillcn@msn.com>
2015-01-29 16:47:43 +0800

    spec

--- api/v1/entities/post.rb
+++ api/v1/entities/post.rb
@@ -7,6 +7,7 @@ module V1
       expose :summary    , documentation: '摘要'
       expose :content    , documentation: '内容'
       expose :title_link , documentation: '标题链接'
+      expose :column_id  , documentation: '专栏编号'
       expose :must_read  , documentation: '必读文章'
       expose :slug       , documentation: '短标题'
       expose :state      , documentation: '状态'
