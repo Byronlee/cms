8eaa5cd6caf0ffed63c3fc620ef1caaf92573bdf
Chad Liu <liuwenmin@36kr.com>
2015-02-02 14:51:24 +0800

    完成添加评论功能

--- db/schema.rb
+++ db/schema.rb
@@ -125,6 +125,7 @@ ActiveRecord::Schema.define(version: 20150204083838) do
     t.string   "role"
   end
 
+
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
