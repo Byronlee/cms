4e8d2469803850af77e02014f4502d99402bfcd8
Chad Liu <liuwenmin@36kr.com>
2015-02-09 11:48:10 +0800

    修证rebase合并时出现的问题

--- app/models/user.rb
+++ app/models/user.rb
@@ -53,8 +53,6 @@ class User < ActiveRecord::Base
     end
   end
 
-  protected
-
   def avatar
     return self.krypton_authentication.info["image"] if self.krypton_authentication && self.krypton_authentication.info["image"].present?
   end
@@ -66,6 +64,8 @@ class User < ActiveRecord::Base
   def may_prepublish?
     [:editor].include? self.role.to_sym
   end
+
+  protected
 
   def email_required?
     false
