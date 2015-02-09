83cad0de38f27f8f4336be55cdc29e4584bedaaa
Chad Liu <liuwenmin@36kr.com>
2015-02-09 16:34:22 +0800

    讲评论和文字相关的js移动到post.js文件

---
+++ app/assets/javascripts/application/post.js
@@ -0,0 +1,42 @@
+function imgError(image) {
+  image.onerror = "";
+  image.src = "/assets/img/girl.jpg";
+  return true;
+}
+
+function toggleCommentForm(){
+  var form = $("#commentForm");
+  var v = form.css('display');
+  if(!v || v == "none"){
+    form.fadeIn();
+    $("#toggleFormIcon").addClass("dropup");
+  }
+  else{
+    form.fadeOut();
+    $("#toggleFormIcon").removeClass("dropup")
+  }
+}
+
+function getNormalList(obj, commentable_type, commentable_id){
+  $.get("/"+ commentable_type +"/"+ commentable_id +"/comments/normal_list.js", function(data, status){
+    if(status == "success"){
+      $(obj).attr("onclick","toggleNormalComments()");
+      toggleNormalComments();
+    }
+  });
+}
+
+function toggleNormalComments(){
+  var normal_list = $("#comments-normal");
+  var v = normal_list.css('display');
+  if(!v || v == "none"){
+    normal_list.fadeIn(function(){
+      $("#toggleNormalComments").text($("#toggleNormalComments").text().replace("Expand", "Collapse"));
+    });
+  }
+  else{
+    normal_list.fadeOut(function(){
+      $('#toggleNormalComments').text($("#toggleNormalComments").text().replace("Collapse", "Expand"));
+    });
+  }
+}
