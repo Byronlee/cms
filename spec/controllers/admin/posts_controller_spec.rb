0f5f55098e71fc53e89b34b09249303c759b2e80
Chad Liu <liuwenmin@36kr.com>
2015-02-09 14:57:05 +0800

    完善post-comment的级联删除功能和测试

--- spec/controllers/admin/posts_controller_spec.rb
+++ spec/controllers/admin/posts_controller_spec.rb
@@ -9,4 +9,18 @@ describe Admin::PostsController do
     end
   end
 
+  describe "DELETE 'destroy'" do
+    let!(:comment){ create(:post_commentable) }
+
+    it "destroy self own comments" do
+      request.env["HTTP_REFERER"] = post_path(comment.commentable)
+      comments = comment.commentable.comments
+      expect{
+        delete :destroy, :id => comment.commentable
+      }.to change(Post, :count).by(-1)
+      expect(comments.blank?).should be_true
+      expect(response).to redirect_to(post_path(comment.commentable))
+    end
+  end
+
 end
