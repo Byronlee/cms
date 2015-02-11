a4da83d3ef981daba8f2c78b33e7afe84ffeec62
Byronlee <lbyronlee@gmail.com>
2015-02-11 14:54:37 +0800

    组件化sidebar

---
+++ app/cells/sidebar_cell.rb
@@ -0,0 +1,25 @@
+class SidebarCell < Cell::Rails
+  def ad
+    render
+  end
+
+  def hot_post
+    render
+  end
+
+  def apps
+    render
+  end
+
+  def excellent_comments
+    render
+  end
+
+  def tags
+    render
+  end
+
+  def social
+    render
+  end
+end
