623d92bd5474ef6108e9d60e2b05d21990cb65bf
chad <chad_lwm@hotmail.com>
2015-02-11 12:24:39 +0800

    add self.down for remove default value of user email

--- db/migrate/20150210052315_remove_default_value_of_blan_string_from_email_on_users.rb
+++ db/migrate/20150210052315_remove_default_value_of_blan_string_from_email_on_users.rb
@@ -2,4 +2,7 @@ class RemoveDefaultValueOfBlanStringFromEmailOnUsers < ActiveRecord::Migration
    def self.up
     change_column :users, :email, :string , :null => true, :default => nil
   end
+
+  def self.down
+  end
 end
