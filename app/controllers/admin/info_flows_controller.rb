5e4451618faf5bae7995f927b4cdb27c7636a34a
chad <chad_lwm@hotmail.com>
2015-02-12 14:24:31 +0800

    完成信息流的修改功能

--- app/controllers/admin/info_flows_controller.rb
+++ app/controllers/admin/info_flows_controller.rb
@@ -76,4 +76,12 @@ class Admin::InfoFlowsController < Admin::BaseController
   def edit
     @columns = Column.all
   end
+
+  def update
+    if params[:column_ids]
+      Column.update_all(in_info_flow:false)
+      Column.where(id:params[:column_ids]).update_all(in_info_flow:true)
+    end
+    render :json => {"result" => "sucess"}
+  end
 end
