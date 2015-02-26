1ecaa44b196474b0d10819072ff7cee0415abc50
chad <chad_lwm@hotmail.com>
2015-02-26 14:53:59 +0800

    信息流名称添加不为空和唯一约束

--- app/models/info_flow.rb
+++ app/models/info_flow.rb
@@ -9,6 +9,10 @@
 #
 
 class InfoFlow < ActiveRecord::Base
+
+  validates :name, presence: true
+  validates_uniqueness_of :name
+
   has_and_belongs_to_many :columns
   has_and_belongs_to_many :ads
 end
