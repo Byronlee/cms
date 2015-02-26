bf244de8df0f951a45258dc4ba282340b5a18f87
chad <chad_lwm@hotmail.com>
2015-02-26 14:48:42 +0800

    ads的position由string转为integer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150210052315) do
+ActiveRecord::Schema.define(version: 20150226064026) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
