4c7a57d46745799cceed5db324954f9584d86aac
Byronlee <lbyronlee@gmail.com>
2015-02-28 17:01:17 +0800

    添加admin root path

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20150226064026) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -76,13 +77,6 @@ ActiveRecord::Schema.define(version: 20150226064026) do
 
   create_table "info_flows", force: true do |t|
     t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "info_flows_columns", force: true do |t|
-    t.integer  "info_flow_id"
-    t.integer  "column_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
