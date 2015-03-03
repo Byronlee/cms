27ec868ad8f9e42927d94d8a5147ed7b691b3cd6
chad <chad_lwm@hotmail.com>
2015-03-03 15:18:39 +0800

    用百度作为站内搜索

--- vendor/assets/javascripts/app.js
+++ vendor/assets/javascripts/app.js
@@ -120,3 +120,12 @@
   });
 
 })(jQuery);
+
+function goSearch(formname)
+{
+  var url = "http://www.baidu.com/baidu";
+  formname.method = "get";
+  document.search_baidu.word.value = document.search_baidu.word_display.value + " site:36kr.com";
+  formname.action = url;
+  return true;
+}
