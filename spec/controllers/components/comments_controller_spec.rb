1de56cbe2a910259695d4761d8a0a8b602b31ef8
chad <chad_lwm@hotmail.com>
2015-03-04 11:54:17 +0800

    添加测试

--- spec/controllers/components/comments_controller_spec.rb
+++ spec/controllers/components/comments_controller_spec.rb
@@ -3,9 +3,10 @@ require 'spec_helper'
 describe Components::CommentsController do
 
   describe "GET 'index'" do
-    it "returns http success" do
+     it "return http success and return the excellent comemnts" do
       get 'index'
       response.should be_success
+      expect(response.header["Content-Type"]).to eq("application/json; charset=utf-8")
     end
   end
 
