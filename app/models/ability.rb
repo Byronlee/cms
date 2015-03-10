9dee434a666a504bc50605f5d017f6833b48cd78
Byronlee <lbyronlee@gmail.com>
2015-03-10 13:54:31 +0800

    弥补简要的权限控制策略

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,22 +2,37 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    if user
-      member(user)
-    else
-      anonymous(user)
-    end
+    public_ability
+    send user.role.to_sym, user if user
   end
 
-  def member(user)
-    can :read, :welcome
-    can :read, :admin
+  # 读者
+  def reader(user)
+  end
+
+  # 投稿者
+  def contributor
+  end
+
+  # 运营
+  def operator(user)
+  end
+
+  # 作者
+  def writer(user)
+  end
+
+  # 编辑
+  def editor
+  end
+
+  # 管理员
+  def admin
     can :manage, :all
   end
 
-  def anonymous(user)
+  def public_ability
     can :read, :welcome
-    can :read, :admin    
-    can :manage, :all
+    can :read, [Ad, Post, Column, Page, Newsflash, User]
   end
 end
