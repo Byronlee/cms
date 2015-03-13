e4cef46da8310cfe80542d00d5e14ff60cdd2702
chad <chad_lwm@hotmail.com>
2015-03-13 10:27:46 +0800

    详情页面处理换行，兼容\r\n \n\n

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,6 @@
 module PostsHelper
   def display_post_content(content, cover_real_url)
-    content_arr = content.split("\n\n")
+    content_arr = content.gsub("\r\n", "\n").split("\n\n")
     return content unless content_arr.first.include? cover_real_url
     content_arr[1..-1].join("\n\n")
   end
