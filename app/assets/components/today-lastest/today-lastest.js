c05e8ab53374ac47cc2ffb12dba8b5a485aa1873
chad <chad_lwm@hotmail.com>
2015-03-16 02:31:12 +0800

    解决问题：polymer中渲染title的html格式

--- app/assets/components/today-lastest/today-lastest.js
+++ app/assets/components/today-lastest/today-lastest.js
@@ -3,8 +3,14 @@
 $.get('/components/posts/today_lastest', {}, function(result){
   Polymer('today-lastest', {
     ready: function(){
-      this.posts = result[0].posts;
-      this.posts_count = result[0].posts_count;
+      if(result){
+        this.posts = result[0].posts;
+        this.posts_count = result[0].posts_count;
+      }
+      else{
+        this.posts = [];
+        this.posts_count = 0;
+      }
      }
   });
 });
