112c5ce48f2016b4942d1c736901717cb225a5e2
chad <chad_lwm@hotmail.com>
2015-03-15 17:20:23 +0800

    投稿者可以查看Dashboard

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,25 +1,36 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(user)
-    public_ability
+  def initialize(user, controller_namespace)
+    unless controller_namespace == 'Admin'
+      public_ability
+      can :create, Comment if User
+    end
     send user.role.to_sym, user if user
   end
 
   # 读者
   def reader(user)
+    # 不能进入后台
+    # 可以评论
   end
 
   # 投稿者
-  def contributor
+  def contributor(user)
+    can :read, :dashboard
   end
 
   # 运营
   def operator(user)
+    # 头条（all）
+    # 用户 (reader)
+    # 评论
+    # 文章(只看)
   end
 
   # 作者
   def writer(user)
+    #不能进入后台， 可以评论
   end
 
   # 编辑
@@ -32,9 +43,8 @@ class Ability
   end
 
   def public_ability
-    # can :read, :welcome
-    # can :read, [Ad, Post, Column, Page, Newsflash, User, Comment]
-    # can :update_views_count, Post
-    can :manage, :all
+     can :read, :welcome
+     can :read, [Ad, Post, Column, Page, Newsflash, User, Comment]
+     can :update_views_count, Post
   end
 end
