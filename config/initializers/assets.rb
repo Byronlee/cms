6eb8a6885446a77a2a4a123ccf9b6c33f3134215
Byronlee <lbyronlee@gmail.com>
2015-03-17 00:18:12 +0800

    更改assets文件引入方式

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,18 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( admin.css admin.js )
-Rails.application.config.assets.precompile += %w( matrix_admin/matrix_admin.css matrix_admin/matrix_admin.js )
+Rails.application.config.assets.precompile += %w( matrix_admin/matrix_admin.css matrix_admin/matrix_admin.js
+                                                  ScrollToFixed/jquery-scrolltofixed-min.js
+                                                  respond/dest/respond.min.js
+                                                  lazyloadxt/dist/jquery.lazyloadxt.min.js
+                                                  opensans/OpenSansLight.woff
+                                                  opensans/OpenSans.woff
+                                                  opensans/OpenSansBold.woff
+                                                  36kr/36kr.eot
+                                                  36kr/36kr.svg
+                                                  36kr/36kr.ttf
+                                                  36kr/36kr.woff
+                                                  ladda/dist/ladda.min.js
+                                                  ladda/dist/spin.min.js
+                                                  magnific-popup/dist/jquery.magnific-popup.min.js
+                                                  app.js )
