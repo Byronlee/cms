ff015675de15078913e4be59847c402d530b88b8
tian-xiaobo <peterwillcn@msn.com>
2015-03-17 00:23:59 +0800

    部署脚本

--- config/deploy/online.rb
+++ config/deploy/online.rb
@@ -1,3 +1,3 @@
 set :deploy_to, -> { "/var/www/apps/krypton" }
-server '119.254.100.96', roles: %w[web app db], port: 52221, primary: true #, sidekiq: true, whenever: true
-server '119.254.100.96', roles: %w{web app}, port: 52222
+server 'www-data@119.254.100.96', roles: %w[web app db], port: 52221, primary: true #, sidekiq: true, whenever: true
+server 'www-data@119.254.100.96', roles: %w{web app}, port: 52222
