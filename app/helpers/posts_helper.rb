2d94d2adcb2df4004839f8b75c165f7d9e0400cc
chad <chad_lwm@hotmail.com>
2015-03-23 10:51:52 +0800

    修改文章内容中首航含头图的处理策略

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,8 +1,8 @@
 module PostsHelper
   def display_post_content(content, cover_real_url)
-    return content if cover_real_url.blank?
-    content_arr = content.gsub("\r\n", "\n").split("\n")
-    return content unless content_arr.first.include? cover_real_url
-    content_arr[1..-1].join("\n")
+    return content if cover_real_url.blank? || content.blank?
+    sub_str = content[0...cover_real_url.length + 300]
+    return content unless sub_str.include? cover_real_url
+    sub_str.gsub(cover_real_url, '') + content[(cover_real_url.length + 300)..-1]
   end
 end
