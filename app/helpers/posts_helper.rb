c991157cb69e410c94df742c18b34a10a0731a9d
chad <chad_lwm@hotmail.com>
2015-03-23 14:52:54 +0800

    修正文章内容拼接时nil错误

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,6 +3,6 @@ module PostsHelper
     return content if cover_real_url.blank? || content.blank?
     sub_str = content[0...cover_real_url.length + 300]
     return content unless sub_str.include? cover_real_url
-    sub_str.sub(/<img.*?#{cover_real_url}.*?>/, '') + content[(cover_real_url.length + 300)..-1]
+    "#{sub_str.sub(/<img.*?#{cover_real_url}.*?>/, '')}#{content[(cover_real_url.length + 300)..-1]}"
   end
 end
