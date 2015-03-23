9d794bbb95bbaa54c0dc1f7f1a84c7722815eed0
Byronlee <lbyronlee@gmail.com>
2015-03-23 23:47:16 +0800

    修复bug

--- app/models/post.rb
+++ app/models/post.rb
@@ -138,7 +138,7 @@ class Post < ActiveRecord::Base
   end
 
   def generate_url_code
-    self.update(url_code: (self.id + Settings.url_code_begin )) if self.url_code.blank?
+    self.update(url_code: (self.id + 33_000)) if self.url_code.blank?
   end
 
   def check_head_line_cache
