06a8130830206e5aabcac2c8ff86173f76104085
bevis <i@bevis.me>
2015-03-25 12:44:27 +0800

    hotfix: 修改 PostsHelper#display_post_content 使返回的结果不再包含 style；（并 touch 内容页使缓存失效）

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,8 +1,18 @@
 module PostsHelper
   def display_post_content(content, cover_real_url)
-    return content if cover_real_url.blank? || content.blank?
-    sub_str = content[0...cover_real_url.length + 300]
-    return content unless sub_str.include? cover_real_url
-    "#{sub_str.sub(/<img.*?#{cover_real_url}.*?>/, '')}#{content[(cover_real_url.length + 300)..-1]}"
+    content = if cover_real_url.blank? || content.blank?
+      content
+    else
+      sub_str = content[0...cover_real_url.length + 300]
+      unless sub_str.include? cover_real_url
+        content
+      else
+        "#{sub_str.sub(/<img.*?#{cover_real_url}.*?>/, '')}#{content[(cover_real_url.length + 300)..-1]}"
+      end
+    end
+
+    doc = Nokogiri::HTML::DocumentFragment.parse(content)
+    doc.xpath('@style|.//@style').remove
+    doc.to_html
   end
 end
