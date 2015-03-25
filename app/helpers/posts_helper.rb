c5b130e4d7b7fb3a77b99ef7e8253dfac3544c42
Byronlee <lbyronlee@gmail.com>
2015-03-25 13:58:41 +0800

    正确过滤掉正文中无用的标签和属性

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -10,23 +10,12 @@ module PostsHelper
                   "#{sub_str.sub(/<img.*?#{cover_real_url}.*?>/, '')}#{content[(cover_real_url.length + 300)..-1]}"
                 end
               end
-
-    #doc = Nokogiri::HTML::DocumentFragment.parse(content)
-    #doc.xpath('@style|.//@style').remove
-    sanitize_tags(doc).to_html
+    sanitize_tags(content)
   end
 
-  def sanitize_tags(text,namespace=nil)
-    text = text.gsub(/\r\n/,"").gsub(/(^(<p>&nbsp;<\/p>){1,})|((<p>&nbsp;<\/p>){1,}$)/,"")
-    tags = case namespace
-           when 'submitter','online','prefile'
-             %w(p u s br strong em)
-           when 'process'
-             %w(p u s strong br del ins em)
-           else
-             %w(a p br hr i em strong iframe embed)
-           end
-    view_context.sanitize(text, tags: tags, attributes: %w(style class))
+  def sanitize_tags(text)
+    tags = %w(a p br hr i em strong iframe embed img h1 h2 h3 h4 h5)
+    sanitize(text, tags: tags, attributes: %w(href ref target src title alt))
   end
 
 end
