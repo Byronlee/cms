18e4524d3f84fe1ebdab22f14a74c3b938af820e
chad <chad_lwm@hotmail.com>
2015-03-25 21:13:20 +0800

    修改kr+消息不显示问题

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def messages
-    return if params['data']['code'].to_i != 0
+    return render :text => '', layout: false if params['data'].blank? || params['data']['code'].to_i != 0
     render '_messages', layout: false
   end
 end
