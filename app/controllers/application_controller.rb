d487543282fc7d3d765d65252a3e9d54c9c04e54
bevis <i@bevis.me>
2015-03-26 23:44:07 +0800

    先 redirect_to_no_subdomain 后 match_krid_online_status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  prepend_before_action :redirect_to_no_subdomain
+  prepend_before_action :match_krid_online_status, unless: -> { devise_controller? }
+  prepend_before_action :redirect_to_no_subdomain # i'll run first
+
   def redirect_to_no_subdomain
     if request.path !~ /^(api|feed).*/ && (subdomain = request.subdomain).present?
       redirect_to request.original_url.sub("#{subdomain}.36kr.com", '36kr.com')
@@ -14,21 +16,18 @@ class ApplicationController < ActionController::Base
     end
   end
 
-   prepend_before_filter :match_krid_online_status
-   skip_before_filter :match_krid_online_status, if: -> { devise_controller? }
-
-   def match_krid_online_status
-     if (cookie_version = cookies[Settings.oauth.krypton.cookie.name]).present?
-       if !current_user || # 在线状态不同步
-         cookie_version.to_i != current_user.krypton_authentication.version || # 资料版本不同步
-         Time.now > current_user.krypton_authentication.updated_at + 1.hour # 定时更新解决跨浏览器更新资料
-         session["omniauth.ok_url"] = request.fullpath
-         redirect_to user_omniauth_authorize_path(provider: :krypton)
-       end
-     elsif current_user
-       sign_out(current_user)
-     end
-   end
+  def match_krid_online_status
+    if (cookie_version = cookies[Settings.oauth.krypton.cookie.name]).present?
+      if !current_user || # 在线状态不同步
+        cookie_version.to_i != current_user.krypton_authentication.version || # 资料版本不同步
+        Time.now > current_user.krypton_authentication.updated_at + 1.hour # 定时更新解决跨浏览器更新资料
+        session["omniauth.ok_url"] = request.fullpath
+        redirect_to user_omniauth_authorize_path(provider: :krypton)
+      end
+    elsif current_user
+      sign_out(current_user)
+    end
+  end
 
   rescue_from CanCan::AccessDenied do |ex|
     if request.env['HTTP_REFERER']
