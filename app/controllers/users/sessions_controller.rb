da450c9b091f140c11aec9157a2b5e5ebc9d39c6
Byronlee <lbyronlee@gmail.com>
2015-03-26 16:54:50 +0800

    hotfix: 登录重定向

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,9 @@
 class Users::SessionsController < Devise::SessionsController
   def new
-    redirect_to user_omniauth_authorize_path(provider: :krypton)
+    if current_user
+      redirect_to after_sign_in_path_for(current_user)
+    else
+      redirect_to user_omniauth_authorize_path(provider: :krypton)
+    end
   end
 end
