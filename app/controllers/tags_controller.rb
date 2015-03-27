e3289755ea968e7f4015f54933a297ee9020c899
Byronlee <lbyronlee@gmail.com>
2015-03-27 18:56:57 +0800

    没有posts的tag跳转到404

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,8 +6,7 @@ class TagsController < ApplicationController
       .order('published_at desc')
       .includes(:column, author:[:krypton_authentication])
       .page(params[:page]).per(15)
-
-    return redirect_to root_path if @posts.blank?
+    return redirect_to apology_errors_path if @posts.blank?
     @weekly_hot_posts = Post.by_week.tagged_with(params[:tag]).order('views_count desc').limit 15
     @posts_today_lastest = Post.today_lastest
   end
