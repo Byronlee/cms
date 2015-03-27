bca1c1670071b8cf5b8914fb5b007e7883578011
chad <chad_lwm@hotmail.com>
2015-03-27 22:33:40 +0800

    我的收藏只显示发布状态的文章

--- app/controllers/admin/favorites_controller.rb
+++ app/controllers/admin/favorites_controller.rb
@@ -2,7 +2,9 @@ class Admin::FavoritesController < Admin::BaseController
   load_and_authorize_resource
 
   def index
-    @favorites = current_user.favorites.order('id desc').includes({post: :column}, :user).page params[:page]
+    @favorites = current_user.favorites
+                .joins("inner join posts on posts.url_code = favorites.url_code and posts.state = 'published'")
+                .order('id desc').includes({post: :column}, :user).page params[:page]
   end
 
   def destroy
