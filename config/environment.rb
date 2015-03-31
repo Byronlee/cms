0ffdf93c5b0e6382c719ef7fe31554143255bd78
chad <chad_lwm@hotmail.com>
2015-03-31 14:09:33 +0800

    post 详情页应用page view

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+require File.expand_path('../../lib/page_view/page_view', __FILE__)
