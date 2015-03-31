ac874ec457174effd440b5e382ddceb93a811265
tian-xiaobo <peterwillcn@msn.com>
2015-03-31 15:15:15 +0800

    修改seed安全初始化数据

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
-ActiveRecord::Base.connection.execute("select setval('newsflash_topic_colors_id_seq', 1, false)")
-NewsflashTopicColor.delete_all
+#ActiveRecord::Base.connection.execute("select setval('newsflash_topic_colors_id_seq', 1, false)")
+#NewsflashTopicColor.delete_all
+
 site_colors = [
   { site_name: 'Google',   color: '#ddd'  },
   { site_name: 'Baidu',    color: '#000'  },
@@ -9,12 +10,18 @@ site_colors = [
   { site_name: '阿里巴巴',  color: 'blue' }
 ]
 
-site_colors.each do | site |
-  NewsflashTopicColor.create site
+site_colors.each do |site|
+  NewsflashTopicColor.find_or_create_by(site_name: site[:site_name]) do |t|
+    t.color = site[:color]
+  end
 end
 
-ActiveRecord::Base.connection.execute("select setval('columns_id_seq', 1, false)")
-Column.delete_all
+#site_colors.each do | site |
+#  NewsflashTopicColor.create site
+#end
+
+#ActiveRecord::Base.connection.execute("select setval('columns_id_seq', 1, false)")
+#Column.delete_all
 columns = [
   { name:  '国外资讯', slug:    'breaking'},
   { name:  '创业公司', slug:    'startup-type'},
@@ -35,12 +42,18 @@ columns = [
   { name:  '36Kr' , slug:   '36kr'}
 ]
 
-columns.each do | column |
-  Column.new(column).save(:validate => false)
+columns.each do |column|
+  Column.find_or_create_by(name: column[:name]) do |t|
+    t.name = column[:name]
+    t.slug = column[:slug]
+  end
 end
+#columns.each do | column |
+#  Column.new(column).save(:validate => false)
+#end
 
-ActiveRecord::Base.connection.execute("select setval('pages_id_seq', 1, false)")
-Page.delete_all
+#ActiveRecord::Base.connection.execute("select setval('pages_id_seq', 1, false)")
+#Page.delete_all
 pages = [
   { title: '36氪投稿细则', slug: 'contribute',  body: Settings.contribute },
   { title: '关于36氪',    slug: 'about',       body: Settings.about },
@@ -48,9 +61,13 @@ pages = [
   { title: '氪月报',      slug: 'krmonthly',   body: Settings.krmonthly }
 ]
 
-pages.each do | page |
-  Page.create!(page)
+pages.each do |page|
+  Page.find_or_create_by(title: page[:title]) do |t|
+    t.slug = page[:slug]
+    t.body = page[:body]
+  end
 end
-
-
+#pages.each do | page |
+#  Page.create!(page)
+#end
 
