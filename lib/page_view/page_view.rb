964aeda12dab178049a32abfa4f513d75e5cf437
chad <chad_lwm@hotmail.com>
2015-03-31 14:08:35 +0800

    添加page_view 基础模块

--- lib/page_view/page_view.rb
+++ lib/page_view/page_view.rb
@@ -1,45 +1,52 @@
 module PageView
   module ActiveRecord
-    def page_view_field(fields)
-      fields = [] << fields unless fields.is_a? Array
+    def page_view_field(*fields)
       fields.each do |field|
         field = field.to_sym unless field.is_a? Symbol
-        class_eval <<-METHOD_MSG
+
+        class_eval <<-RUBY
           def increase_#{field}
-            filed_name = /^increase_(.*)/i.match(__method__.to_s)[1]
-            cache_key = self.class.to_s+'#'+self.id.to_s+'#'+ filed_name
+            field_name = :#{field}
+            cache_key = get_cache_key(field_name)
             views_count = Redis::HashKey.new('page_views')[cache_key]
-            if views_count.nil?
-              views_count = self.#{field}.to_i
-            else
+            if views_count
               views_count = views_count.to_i
+            else
+              views_count = self.#{field}.to_i
             end
             Redis::HashKey.new('page_views')[cache_key] = views_count.next
           end
 
           def persist_#{field}(options = {})
-            filed_name = /^persist_(.*)/i.match(__method__.to_s)[1]
-            cache_key = self.class.to_s+'#'+self.id.to_s+'#'+ filed_name
+            field_name = :#{field}
+            cache_key = get_cache_key(field_name)
             views_count = Redis::HashKey.new('page_views')[cache_key]
             if views_count.present? && self.#{field}.to_i < views_count.to_i
-              unless options[:skip_callbacks]
-                self.update_attribute(filed_name.to_sym, views_count.to_i)
+              if options[:skip_callbacks]
+                self.update_column(field_name, views_count.to_i)
               else
-                self.update_column(filed_name.to_sym, views_count.to_i)
+                self.update_attribute(field_name, views_count.to_i)
               end
+              real_value = views_count.to_i
             else
               Redis::HashKey.new('page_views')[cache_key] = self.#{field}.to_i
+              real_value = self.#{field}.to_i
             end
+            real_value
           end
 
           def cache_#{field}
-            filed_name = /^cache_(.*)/i.match(__method__.to_s)[1]
-            cache_key = self.class.to_s+'#'+self.id.to_s+'#'+ filed_name
+            field_name = :#{field}
+            cache_key = get_cache_key(field_name)
             views_count = Redis::HashKey.new('page_views')[cache_key]
-            views_count || self.#{field}.to_i
+            (views_count.presence || self.#{field}).to_i
           end
 
-        METHOD_MSG
+          def get_cache_key(field_name)
+            self.class.to_s + '#' + self.id.to_s + '#' + field_name.to_s
+          end
+        RUBY
+
       end
     end
   end
