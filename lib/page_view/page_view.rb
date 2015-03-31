6793a1bf950bec952583df46671114714f537cb4
chad <chad_lwm@hotmail.com>
2015-03-31 14:47:26 +0800

    添加persist 函数

--- lib/page_view/page_view.rb
+++ lib/page_view/page_view.rb
@@ -8,14 +8,24 @@ module PageView
           def increase_#{field}
             filed_name = /^increase_(.*)/i.match(__method__.to_s)[1]
             cache_key = self.class.to_s+'#'+self.id.to_s+'#'+ filed_name
-            cache_value = Redis::HashKey.new('page_views')[cache_key]
-            views_count = cache_value
+            views_count = Redis::HashKey.new('page_views')[cache_key]
             if views_count.nil?
               views_count = self.#{field}.to_i
             else
               views_count = views_count.to_i
             end
             Redis::HashKey.new('page_views')[cache_key] = views_count.next
+          end
+
+          def persist_#{field}
+            filed_name = /^persist_(.*)/i.match(__method__.to_s)[1]
+            cache_key = self.class.to_s+'#'+self.id.to_s+'#'+ filed_name
+            views_count = Redis::HashKey.new('page_views')[cache_key]
+            if views_count.present? && self.#{field}.to_i < views_count.to_i
+              self.update_attribute(filed_name.to_sym, views_count.to_i)
+            else
+              Redis::HashKey.new('page_views')[cache_key] = self.#{field}.to_i
+            end
           end
         METHOD_MSG
       end
