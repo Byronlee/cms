fbc4941dd5721ec5541a2dae24c08c7bf46a4765
chad <chad_lwm@hotmail.com>
2015-04-01 13:49:31 +0800

    删除搜索结果页的form

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,10 @@
 class SearchController < ApplicationController
   def search
-    if params[:q].nil?
-      @articles = []
-    else
+    if params[:q]
       options={page: 1, per_page: 10}
       @articles = Post.search(params[:q],options)
+    else
+      @articles = []
     end
   end
 end
