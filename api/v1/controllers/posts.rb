9a8b54adfbe7bb22cbc27122b6a81d49ff4f309a
tian-xiaobo <peterwillcn@msn.com>
2015-04-09 13:34:30 +0800

    添加回复接口

--- api/v1/controllers/posts.rb
+++ api/v1/controllers/posts.rb
@@ -70,6 +70,45 @@ module V1
           present posts, with: Entities::Post
         end
 
+        desc 'get post comments list'
+        params do
+          optional :type, type: String, default: 'post', desc: '多态类型'
+          optional :page,  type: Integer, default: 1, desc: '页数'
+          optional :per_page,  type: Integer, default: 30, desc: '每页记录数'
+          #optional :action,  type: String, default: 'down', desc: '下翻页 down 和 上翻页 up'
+        end
+        get ':id/replies' do
+          #post = params[:type].classify.constantize.find_by_url_code params[:id]
+          post = Post.where(url_code: params[:id]).first
+          comments = post.comments.includes(:commentable, user:[:krypton_authentication])
+          .order('created_at desc').page(params[:page]).per(params[:per_page])
+          #present @comments, with: Entities::Comment
+          replies_list = []
+          comments.each do |replie|
+           replies_list << {
+             id: replie.id,
+             topic_id: post.url_code,
+             message_id: nil,
+             created_at: replie.created_at.iso8601,
+             updated_at: replie.updated_at.iso8601,
+             body: replie.content,
+             body_html: replie.content,
+           }.merge(
+             user: { id: replie.user.id,
+               login: replie.user.name,
+               name: replie.user.name,
+               email: replie.user.email,
+               avatar_url: replie.user.avatar
+             }) if replie.user.id != 10002
+          end
+          { replies: replies_list }.merge({
+            id: post.url_code,
+            title: post.title,
+            replied_at: comments.last.created_at.iso8601,
+            replies_count: comments.size
+          })
+        end
+
         desc 'get id posts for page list'
         params do
           optional :page,  type: Integer, default: 1, desc: '页数'
