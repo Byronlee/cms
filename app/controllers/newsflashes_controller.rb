5a4f206e08d6f413b68198fd6eba9af3c1b440d2
chad <chad_lwm@hotmail.com>
2015-04-14 13:57:46 +0800

    通过 id 访问，转入当天的资讯列表页面（利用锚点跳到当前位置

--- app/controllers/newsflashes_controller.rb
+++ app/controllers/newsflashes_controller.rb
@@ -5,5 +5,8 @@ class NewsflashesController < ApplicationController
   end
 
   def show
+    newsflash = Newsflash.find params[:id]
+    date = newsflash.created_at
+    redirect_to newsflashes_of_day_path(year: date.year, month: date.month, day: date.day, anchor: newsflash.id)
   end
 end
