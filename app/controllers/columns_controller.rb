336bdab579b771d121418a275958a1aae948c81c
Byronlee <lbyronlee@gmail.com>
2015-04-14 15:56:33 +0800

    将一周热门文章放到cell

--- app/controllers/columns_controller.rb
+++ app/controllers/columns_controller.rb
@@ -1,18 +1,8 @@
 class ColumnsController < ApplicationController
-  def index
-  end
-
   def show
-    @column = Column.find_by_slug(params[:slug])
-    if @column
-      @posts = @column.posts.published
-        .order('published_at desc')
-        .includes(:column, author:[:krypton_authentication])
-        .page(params[:page]).per(15)
-      @weekly_hot_posts = @column.posts.by_week.order('views_count desc').limit 15
-    else
-      @posts = []
-      @weekly_hot_posts = []
-    end
+    @column = Column.find_by_slug!(params[:slug])
+    @posts = @column.posts.published.order('published_at desc')
+    @posts = @posts.includes(:column, author: [:krypton_authentication])
+    @posts = @posts.page(params[:page]).per(15)
   end
 end
