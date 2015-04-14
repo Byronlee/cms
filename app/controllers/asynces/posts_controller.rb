01c62c85b34272f9696b11ca209602bff10e73b8
Byronlee <lbyronlee@gmail.com>
2015-04-14 13:14:22 +0800

    将redis HashKey 封装一下

--- app/controllers/asynces/posts_controller.rb
+++ app/controllers/asynces/posts_controller.rb
@@ -1,6 +1,6 @@
 class Asynces::PostsController < ApplicationController
   def hots
-    posts_data = Redis::HashKey.new('posts')['hot_posts']
+    posts_data =  CacheClient.instance.hot_posts
     @posts = posts_data.present? ? JSON.parse(posts_data) : []
     render '_hots', :layout => false
   end
