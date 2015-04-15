69d29e63e121305bcd66b99ee541645f47932f8b
Byronlee <lbyronlee@gmail.com>
2015-04-15 14:22:41 +0800

    hotfix: user controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,12 +1,10 @@
 require 'spec_helper'
 
 describe UsersController do
-
   describe "GET 'messages'" do
-    it "returns http success" do
+    it 'returns http success' do
       get 'messages'
       response.should be_success
     end
   end
-
 end
