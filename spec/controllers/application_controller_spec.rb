d1510e74911f76d916b720f5ef77966488a5090f
Byronlee <lbyronlee@gmail.com>
2015-04-15 13:33:52 +0800

    格式化search controller测试代码

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,74 @@
+require 'spec_helper'
+
+describe ApplicationController do
+  include Rails.application.routes.url_helpers
+  login_user
+
+  describe '#redirect_to_no_subdomain', :type => :request do
+    describe 'when request path include api or feed it should direct request' do
+      it 'www and feed' do
+        get 'http://www.kid.36kr.com/feed'
+        expect(response).to be_success
+        expect(response.status).to eq 200
+      end
+
+      it 'www and api' do
+        get 'http://www.2015.36kr.com/api/a14f30b8405857de59e098af4d1d07bda752a2dc'
+        expect(response.status).to eq 200
+        expect(response).to be_success
+      end
+
+      it 'only feed' do
+        get 'http://kid.36kr.com/feed'
+        expect(response.status).to eq 200
+        expect(response).to be_success
+      end
+
+      it 'only api' do
+        get 'http://2015.36kr.com/api/a14f30b8405857de59e098af4d1d07bda752a2dc'
+        expect(response.status).to eq 200
+        expect(response).to be_success
+      end
+    end
+
+    describe 'www request not include api or feed, redirect_to a domain not include www' do
+      it 'www' do
+        get 'http://www.36kr.com'
+        expect(response).to redirect_to('http://36kr.com/')
+      end
+    end
+  end
+
+  describe '#match_krid_online_status', :type => :request do
+    context 'cookies not present' do
+      before do
+        cookies[Settings.oauth.krypton.cookie.name] = nil
+      end
+
+      it 'should redirect_to sign_out path' do
+        get 'http://kid.36kr.com'
+        # expect(response).to redirect_to(sign_out(assigns(:current_user)))
+        expect(response).to be_success
+      end
+    end
+  end
+
+  describe 'handling AccessDenied exceptions' do
+    controller do
+      def index
+        raise CanCan::AccessDenied
+      end
+    end
+
+    it 'redirects to the errors#forbidden page' do
+      get :index
+      expect(response).to render_template('errors/forbidden')
+    end
+
+    it 'redirects to the /401.html page if not sigin' do
+      sign_out(session_user)
+      get :index
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe PagesController do
-
   describe "GET 'show'" do
     let(:page) { create :page }
 
