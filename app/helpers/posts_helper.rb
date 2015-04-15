5cccf026df400eb35e792f37fe3bb16498307a32
chad <chad_lwm@hotmail.com>
2015-04-15 17:10:23 +0800

    文章内容显示时去除空行

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,7 @@
 module PostsHelper
   def display_post_content(content, cover_real_url)
-    content = if cover_real_url.blank? || content.blank?
+    return content if content.blank?
+    content = if cover_real_url.blank?
                 content
               else
                 sub_str = content[0...cover_real_url.length + 300]
@@ -10,13 +11,18 @@ module PostsHelper
                   "#{sub_str.sub(/<img.*?#{cover_real_url}.*?>/, '')}#{content[(cover_real_url.length + 300)..-1]}"
                 end
               end
-    sanitize_tags(content)
+     content = sanitize_tags(content)
+     remove_blank_lines(content)
   end
 
   def sanitize_tags(text)
     tags = %w(a p br hr i em u strong iframe embed blockquote img h1 h2 h3 h4 h5 ul li ol)
     attributes = %w(href ref target src title alt width height frameborder allowfullscreen)
     sanitize(text, tags: tags, attributes: attributes)
+  end
+
+  def remove_blank_lines(text)
+    text.to_s.gsub('<p><br></p>', '')
   end
 
   def onblur_title(title)
