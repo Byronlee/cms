7ea396f53c6361b69abf22781acce2eeb933313a
Byronlee <lbyronlee@gmail.com>
2015-04-20 17:34:40 +0800

    noting

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,31 +2,31 @@
 #
 # Table name: posts
 #
-#  id             :integer          not null, primary key
-#  title          :string(255)
-#  summary        :text
-#  content        :text
-#  title_link     :string(255)
-#  must_read      :boolean
-#  slug           :string(255)
-#  state          :string(255)
-#  draft_key      :string(255)
-#  column_id      :integer
-#  user_id        :integer
-#  created_at     :datetime
-#  updated_at     :datetime
-#  cover          :text
-#  source         :string(255)
-#  comments_count :integer
-#  md_content     :text
-#  url_code       :integer
-#  views_count    :integer          default(0)
-#  catch_title    :text
-#  published_at   :datetime
-#  key            :string(255)
-#  remark         :text
-#  extra          :text
-#  source_type    :string(255)
+#  id              :integer          not null, primary key
+#  title           :string(255)
+#  summary         :text
+#  content         :text
+#  title_link      :string(255)
+#  must_read       :boolean
+#  slug            :string(255)
+#  state           :string(255)
+#  draft_key       :string(255)
+#  column_id       :integer
+#  user_id         :integer
+#  created_at      :datetime
+#  updated_at      :datetime
+#  cover           :text
+#  source          :string(255)
+#  comments_count  :integer
+#  md_content      :text
+#  url_code        :integer
+#  views_count     :integer          default(0)
+#  catch_title     :text
+#  published_at    :datetime
+#  key             :string(255)
+#  remark          :text
+#  extra           :text
+#  source_type     :string(255)
 #  favorites_count :integer
 #
 
