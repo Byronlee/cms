c988fb4d383ddb22ba9bccd80d6b46ef84185af7
chad <chad_lwm@hotmail.com>
2015-04-20 13:09:02 +0800

    添加测试

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -5,4 +5,9 @@ describe WelcomeController do
     before { get :index }
     it { should respond_with(:success) }
   end
+
+  describe "GET 'changes'" do
+    before { get :changes}
+    it { should respond_with(:success) }
+  end
 end
