16032d780e2f05d7483ebecf1f145ee25db2be6b
Byronlee <lbyronlee@gmail.com>
2015-04-22 11:35:12 +0800

    AdminUI: 修正发布文章的陈列列表形式

--- app/helpers/admin/posts_helper.rb
+++ app/helpers/admin/posts_helper.rb
@@ -2,11 +2,11 @@ module Admin::PostsHelper
   def display_state(state)
     case state
     when 'reviewing'
-      raw "<span class='badge badge-warning' title='待审查'><i class='fa fa-question'></i></span>"
+      raw "<span title='待审查'><i class='fa fa-question'></i></span>"
     when 'published'
-      raw "<span class='badge badge-success' title='已发布'><i class='fa fa-check'></i></span>"
+      raw "<span title='已发布'><i class='fa fa-check'></i></span>"
     else
-      raw "<span class='badge badge-important' title='错误状态'><i class='fa fa-remove'></i></span>"
+      raw "<span title='错误状态'><i class='fa fa-remove'></i></span>"
     end
   end
 end
