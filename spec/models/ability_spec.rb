f7e17791745480c9f6a586d5ee4a3f9e7a62b683
chad <chad_lwm@hotmail.com>
2015-04-27 16:54:39 +0800

    完善ability测试

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -2,12 +2,19 @@ require "spec_helper"
 require "cancan/matchers"
 
 describe Ability do
-  subject { Ability.new user, nil }
 
-  context "anonymous" do
+  shared_context "public ability" do
+    subject { Ability.new user, '' }
     let(:user) { nil }
+  end
+
+  shared_context "admin ability" do
+    subject { Ability.new user, 'Admin' }
+    let(:user) { nil }
+  end
+
+  shared_examples "has public ability" do
     it do
-      should be_able_to :index, :welcome
       should be_able_to :read, :welcome
       should be_able_to :read, Ad
       should be_able_to :read, Post
@@ -24,124 +31,276 @@ describe Ability do
 
       should be_able_to :read, Comment
       should be_able_to :execllents, Comment
+    end
+  end
 
-      should_not be_able_to :create, Comment
-      should_not be_able_to :read, :dashboard
+  context "anonymous" do
+    context "public page" do
+      include_context "public ability"
+
+      it_behaves_like 'has public ability'
+
+      it do
+        should_not be_able_to :create, Comment
+        should_not be_able_to :read, :dashboard
+        should_not be_able_to :read, Favorite
+        should_not be_able_to :new, Post
+      end
+    end
+
+    context "admin page" do
+      include_context "admin ability"
+
+      it do
+        should_not be_able_to :read, :dashboard
+        should_not be_able_to :read, Ad
+        should_not be_able_to :read, Post
+        should_not be_able_to :read, Column
+        should_not be_able_to :read, Page
+        should_not be_able_to :read, Newsflash
+        should_not be_able_to :read, User
+        should_not be_able_to :read, Comment
+        should_not be_able_to :read, Favorite
+      end
     end
   end
 
   context "reader role" do
-    let(:user) { create :user, :reader }
-    before do
-      @favorite = create :favorite, user_id: user.id
+    context "public page" do
+      include_context "public ability" do
+        let(:user) { create :user, :reader }
+      end
+
+      before do
+        @favorite = create :favorite, user_id: user.id
+      end
+
+      it_behaves_like 'has public ability'
+
+      it do
+        should be_able_to :create, Comment
+        should be_able_to :preview, Post
+        should be_able_to :edit, user
+        should be_able_to :update, user
+        should be_able_to :manage, @favorite
+
+        should_not be_able_to :new, Post
+        should_not be_able_to :read, :dashboard
+      end
+
+      it 'shutup user cannot comment'do
+        user.shutup!
+        should_not be_able_to :create, Comment
+      end
     end
 
-    it do
-      should be_able_to :index, :welcome
-      should be_able_to :create, Comment
-      should be_able_to :preview, Post
-      should be_able_to :edit, user
-      should be_able_to :update, user
-      should be_able_to :manage, @favorite
+    context "admin page" do
+      include_context "admin ability" do
+        let(:user) { create :user, :reader }
+      end
 
-      should_not be_able_to :read, :dashboard
-    end
+      before do
+        @favorite = create :favorite, user_id: user.id
+      end
 
-    it 'shutup user cannot comment'do
-      user.shutup!
-      should_not be_able_to :create, Comment
+      it do
+        should_not be_able_to :read, :dashboard
+        should_not be_able_to :read, Ad
+        should_not be_able_to :read, Post
+        should_not be_able_to :read, Column
+        should_not be_able_to :read, Page
+        should_not be_able_to :read, Newsflash
+        should_not be_able_to :read, Comment
+
+        should be_able_to :update, user
+        should be_able_to :manage, @favorite
+      end
+
     end
   end
 
   context "contributor role" do
-    let(:user) { create :user, :contributor }
+    context "public page" do
+      include_context "public ability" do
+        let(:user) { create :user, :contributor }
+      end
 
-    before do
-      @newsflash = create :newsflash, user_id: user.id
-      @post = create :post, :published, author: user
-      @comment = create :comment
+      before do
+        @newsflash = create :newsflash, user_id: user.id
+        @post = create :post, :published, author: user
+        @comment = create :comment, commentable_id: @post.id, commentable_type: 'Post'
+      end
+
+      it_behaves_like 'has public ability'
+
+      it do
+        should be_able_to :new, Post
+      end
+
+      it do
+        @post.undo_publish
+        @post.save
+        should be_able_to :reviewings, @post
+      end
     end
 
-    it do
-      should be_able_to :read, :dashboard
-      should be_able_to [:read, :create], Newsflash
-      should be_able_to :manage, @newsflash
-      should be_able_to :new, Post
-      should be_able_to :myown, Post
+    context "admin page" do
+      include_context "admin ability" do
+        let(:user) { create :user, :contributor }
+      end
 
-      should be_able_to :read, @post
-      should be_able_to :read, @comment
-    end
+      before do
+        @newsflash = create :newsflash, user_id: user.id
+        @post = create :post, :published, author: user
+        @comment = create :comment, commentable_id: @post.id, commentable_type: 'Post'
+      end
 
-    it do
-      @post.undo_publish
-      @post.save
-      should be_able_to :reviewings, @post
+      it do
+        should be_able_to :read, :dashboard
+        should be_able_to [:read, :create], Newsflash
+        should be_able_to :manage, @newsflash
+        should be_able_to :new, Post
+        should be_able_to :myown, Post
+
+        should be_able_to :read, @post
+        should be_able_to :read, @comment
+      end
     end
   end
 
   context "operator role" do
-    let(:user) { create :user, :operator }
+    context "public page" do
+      include_context "public ability" do
+        let(:user) { create :user, :operator }
+      end
 
-    it do
-      should be_able_to :read, :dashboard
-      should be_able_to :read, User
-      should be_able_to :shutup, User
+      it_behaves_like 'has public ability'
 
-      should be_able_to :read, Post
-      should be_able_to :reviewings, Post
-      should be_able_to :toggle_tag, Post
-      should be_able_to :manage, HeadLine
-      should be_able_to :create, Comment
+      it do
+        should be_able_to :read, :dashboard
+        should be_able_to :read, User
+        should be_able_to :create, Comment
+      end
+    end
+
+    context "admin page" do
+      include_context "admin ability" do
+        let(:user) { create :user, :operator }
+      end
+
+      it do
+        should be_able_to :read, :dashboard
+        should be_able_to :read, User
+        should be_able_to :shutup, User
+
+        should be_able_to :read, Post
+        should be_able_to :reviewings, Post
+        should be_able_to :toggle_tag, Post
+        should be_able_to :manage, HeadLine
+      end
     end
   end
 
   context "writer role" do
-    let(:user) { create :user, :writer }
+    context "public page" do
+      include_context "public ability" do
+        let(:user) { create :user, :writer }
+      end
 
-    before do
-      @post = create :post, :published, author: user
-      @comment = create :comment
+      before do
+        @post = create :post, :published, author: user
+        @comment = create :comment, commentable_id: @post.id, commentable_type: 'Post'
+      end
+
+      it do
+        should be_able_to :read, :dashboard
+      end
     end
 
-    it do
-      should be_able_to :read, :dashboard
-      should be_able_to :read, Post
-      should be_able_to :reviewings, Post
-      should be_able_to :update, @post
-      should be_able_to :destroy, @post
-      should_not be_able_to :toggle_tag, Post
-      should be_able_to :manage, Newsflash
-      should be_able_to :read, @comment
+    context "admin page" do
+      include_context "admin ability" do
+        let(:user) { create :user, :writer }
+      end
+
+      before do
+        @post = create :post, :published, author: user
+        @comment = create :comment, commentable_id: @post.id, commentable_type: 'Post'
+      end
+
+      it do
+        should be_able_to :read, :dashboard
+        should be_able_to :read, Post
+        should be_able_to :reviewings, Post
+        should be_able_to :update, @post
+        should be_able_to :destroy, @post
+        should_not be_able_to :toggle_tag, Post
+        should be_able_to :manage, Newsflash
+        should be_able_to :read, @comment
+      end
     end
   end
 
   context "editor role" do
-    let(:user) { create :user, :editor }
+    context "public page" do
+      include_context "public ability" do
+        let(:user) { create :user, :editor }
+      end
 
-    it do
-      should be_able_to :read, :dashboard
-      should be_able_to :manage, Post
-      should be_able_to :manage, Newsflash
-      should be_able_to :manage, Column
-      should be_able_to :manage, Comment
-      should be_able_to :manage, HeadLine
-      should be_able_to :manage, Page
-      should be_able_to :change_author, Post
-      should_not be_able_to :toggle_tag, Post
+      it_behaves_like 'has public ability'
+
+      it do
+        should be_able_to :read, :dashboard
+      end
+
+      it 'shutup user cannot manage comment'do
+        user.shutup!
+        should_not be_able_to :manage, Comment
+      end
     end
 
-    it 'shutup user cannot manage comment'do
-      user.shutup!
-      should_not be_able_to :manage, Comment
+    context "admin page" do
+      include_context "admin ability" do
+        let(:user) { create :user, :editor }
+      end
+
+      it do
+        should be_able_to :read, :dashboard
+        should be_able_to :manage, Post
+        should be_able_to :manage, Newsflash
+        should be_able_to :manage, Column
+        should be_able_to :manage, Comment
+        should be_able_to :manage, HeadLine
+        should be_able_to :manage, Page
+        should be_able_to :change_author, Post
+        should_not be_able_to :toggle_tag, Post
+      end
+
+      it 'shutup user cannot manage comment'do
+        user.shutup!
+        should_not be_able_to :manage, Comment
+      end
     end
   end
 
   context "admin role" do
-    let(:user) { create :user, :admin }
+    context "public page" do
+      include_context "public ability" do
+        let(:user) { create :user, :admin }
+      end
 
-    it do
-      should be_able_to :manage, :all
+      it do
+        should be_able_to :manage, :all
+      end
+    end
+
+    context "admin page" do
+      include_context "admin ability" do
+        let(:user) { create :user, :admin }
+      end
+
+      it do
+        should be_able_to :manage, :all
+      end
     end
   end
 end
