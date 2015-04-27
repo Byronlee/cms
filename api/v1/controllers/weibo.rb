1ce9738cf21084d5df61b2de25cb51a104724ab0
tian-xiaobo <peterwillcn@msn.com>
2015-04-27 18:09:19 +0800

    只解析文章的url

--- api/v1/controllers/weibo.rb
+++ api/v1/controllers/weibo.rb
@@ -12,34 +12,43 @@ module V1
         get "topics" do
           url = params[:url]
           uri = url.scan(URI.regexp)
-          pid = uri[0][6].match(/\d+/)[0].to_i
-          @post = Post.find_by_url_code pid
-          if !@post.nil? && @post.state == "published"
-            {
-              "display_name" => "#{@post.title}",
-              "image" =>  {
-              "url" => "#{@post.cover_real_url}!s2",
-              "width" => 120,
-              "height" =>  120
-            },
-              "author" =>  {
-              "display_name" =>  "#{@post.author.name||"36Kr"}",
-              "url" => "#{Settings.site}",
-              "object_type" =>  "person"
-            },
-              "summary" =>  "#{@post.summary||""}",
-              "url" =>  "#{Settings.site}/p/#{@post.url_code}.html",
-            "links" =>  {
-              "url" =>  "#{Settings.site}/p/#{@post.url_code}.html",
-              "scheme" =>  "scheme://www.36kr.com/p/#{@post.url_code}",
-              "display_name" =>  "阅读全文"
-            },
-              "tags" =>  [ {
-              "display_name" =>  "#{@post.tag_list.first||""}" }
-            ],
-              "create_at" => "#{@post.created_at||Time.now}",
-              "object_type" =>  "article"
-            }
+          #pid = uri[0][6].match(/\d+/)[0].to_i
+          match_uri = uri[0][6].match(/^\/p\/(\d+)/)
+          unless match_uri.nil?
+            pid = match_uri[1].to_i
+            @post = Post.find_by_url_code pid
+            if !@post.nil? && @post.state == "published"
+              {
+                "display_name" => "#{@post.title}",
+                "image" =>  {
+                "url" => "#{@post.cover_real_url}!s2",
+                "width" => 120,
+                "height" =>  120
+              },
+                "author" =>  {
+                "display_name" =>  "#{@post.author.name||"36Kr"}",
+                "url" => "#{Settings.site}",
+                "object_type" =>  "person"
+              },
+                "summary" =>  "#{@post.summary||""}",
+                "url" =>  "#{Settings.site}/p/#{@post.url_code}.html",
+              "links" =>  {
+                "url" =>  "#{Settings.site}/p/#{@post.url_code}.html",
+                "scheme" =>  "scheme://www.36kr.com/p/#{@post.url_code}",
+                "display_name" =>  "阅读全文"
+              },
+                "tags" =>  [ {
+                "display_name" =>  "#{@post.tag_list.first||""}" }
+              ],
+                "create_at" => "#{@post.created_at||Time.now}",
+                "object_type" =>  "article"
+              }
+            else
+              {
+                "errcode" => "-1",
+                "msg" => "NotFound!"
+              }
+            end
           else
             {
               "errcode" => "-1",
