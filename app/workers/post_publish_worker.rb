7ca7762c60c4738fbfd6d185c6f7d37a3d415c98
chad <chad_lwm@hotmail.com>
2015-04-27 19:21:44 +0800

    edit

--- app/workers/post_publish_worker.rb
+++ app/workers/post_publish_worker.rb
@@ -1,8 +1,8 @@
 class PostPublishWorker < BaseWorker
   def perform(post_id)
     post = Post.find(post_id)
-    return true if post.published? || post.will_publish_at.blank? || Time.parse(post.will_publish_at).past?
-    return true if Time.parse(post.will_publish_at) - Time.now > 120
+    return true if post.published? || post.will_publish_at.blank?
+    return true if post.will_publish_at - Time.now > 120
     post.publish
     post.save
   end
