c234e39084c73fc4b880bc52407fa6b84736f859
chad <chad_lwm@hotmail.com>
2015-04-28 12:06:27 +0800

    修改/api/site_map路由覆盖问题，并完善测试

--- spec/routes/routing_spec.rb
+++ spec/routes/routing_spec.rb
@@ -5,20 +5,24 @@ describe 'Routing' do
 
   describe 'Welcome Controller', :type => :routing do
     context '#changes' do
-      it { expect(:get => '/changes').to route_to(controller: 'welcome', action: 'changes') }
+      it { expect(get: '/changes').to route_to(controller: 'welcome', action: 'changes') }
     end
 
     context '#archive' do
-      it { expect(:get => '/2015').to route_to(controller: 'welcome', action: 'archives', year: '2015') }
-      it { expect(:get => '/2015/04').to route_to(controller: 'welcome', action: 'archives', year: '2015', month: '04') }
-      it { expect(:get => '/2015/04/20').to route_to(controller: 'welcome', action: 'archives', year: '2015', month: '04', day: '20') }
+      it { expect(get: '/2015').to route_to(controller: 'welcome', action: 'archives', year: '2015') }
+      it { expect(get: '/2015/04').to route_to(controller: 'welcome', action: 'archives', year: '2015', month: '04') }
+      it { expect(get: '/2015/04/20').to route_to(controller: 'welcome', action: 'archives', year: '2015', month: '04', day: '20') }
+    end
+
+    context '#site_map' do
+      it{ expect(get: 'api/site_map.xml').to route_to(controller: 'welcome', action: 'site_map', format: 'xml') }
     end
   end
 
   describe 'Newsflashes Controller', :type => :routing do
     context '#index' do
-      it { expect(:get => '/clipped/2015/04/20').to route_to(controller: 'newsflashes', action: 'index', year: '2015', month: '04', day: '20') }
-      it { expect(:get => '/clipped/1234').to route_to(controller: 'newsflashes', action: 'show', id: '1234') }
+      it { expect(get: '/clipped/2015/04/20').to route_to(controller: 'newsflashes', action: 'index', year: '2015', month: '04', day: '20') }
+      it { expect(get: '/clipped/1234').to route_to(controller: 'newsflashes', action: 'show', id: '1234') }
     end
   end
   
