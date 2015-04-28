ccca30c45bb7fb7da062d2fc517b91c7437c21b8
Byronlee <lbyronlee@gmail.com>
2015-04-28 21:36:32 +0800

    hotfix: 修正safari下Dashboar图表不能正常显示

--- app/controllers/asynces/dashboard_controller.rb
+++ app/controllers/asynces/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class Asynces::DashboardController < ApplicationController
   def chats
+    response.headers['Access-Control-Expose-Headers'] = 'Location'
     @sessions = Authentication.today.count
     @posts_data, @users_data, @comments_data = chart_data
     @date_range = (9.days.ago.to_date..3.days.ago).map { |d| d.strftime('%d/%m') }
@@ -9,13 +10,13 @@ class Asynces::DashboardController < ApplicationController
   def pandect
     render 'pandect', layout: false
   end
-  
-  private 
+
+  private
 
   # TODO: 优化查询语句
   # TODO: 补充测试
   def chart_data
-    [Post, User, Comment].map do | obj | 
+    [Post, User, Comment].map do | obj |
        time_interval.inject([]) { |sum, day| sum << obj.by_day(day).count }
      end
   end
