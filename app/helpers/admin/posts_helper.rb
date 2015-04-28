84b1c3b47b3ddfcba69ef43a3d49106b9352a69d
Byronlee <lbyronlee@gmail.com>
2015-04-28 12:00:59 +0800

    在文章管理也显示收藏数量

--- app/helpers/admin/posts_helper.rb
+++ app/helpers/admin/posts_helper.rb
@@ -4,9 +4,9 @@ module Admin::PostsHelper
     when 'reviewing'
       raw "<span title='待审查' style='padding-right: 0;'><i class='fa fa-question'></i></span>"
     when 'published'
-      raw "<span title='已发布'><i class='fa fa-check'></i></span>"
+      raw "<span title='已发布'  style='padding-right: 0;'><i class='fa fa-check'></i></span>"
     else
-      raw "<span title='错误状态'><i class='fa fa-remove'></i></span>"
+      raw "<span title='错误状态'  style='padding-right: 0;'><i class='fa fa-remove'></i></span>"
     end
   end
 end
