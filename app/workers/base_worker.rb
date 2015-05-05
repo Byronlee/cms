1bc0dfbc51ca0ab7d456c2b5d8c7dcf864ed20b7
chad <chad_lwm@hotmail.com>
2015-05-05 11:36:15 +0800

    loged when sidekiq retries

--- app/workers/base_worker.rb
+++ app/workers/base_worker.rb
@@ -1,4 +1,8 @@
 class BaseWorker
   include Sidekiq::Worker
   sidekiq_options :queue => :krx2015, :backtrace => true
+
+  sidekiq_retries_exhausted do |msg|
+    Sidekiq.logger.warn "Failed #{msg['class']} with #{msg['args']}: #{msg['error_message']}"
+  end
 end
