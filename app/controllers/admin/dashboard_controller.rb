a8558b9b3b4001278a03ea4435e4944e580947d8
chad <chad_lwm@hotmail.com>
2015-05-08 12:15:17 +0800

    后台 Dashboard 今日文章按点击量排序

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,6 @@ class Admin::DashboardController < Admin::BaseController
   authorize_object :dashboard
 
   def index
-    @posts = Post.today.published
+    @posts = Post.today.published.includes({ author: :krypton_authentication }, :column, :tags).order("views_count desc")
   end
 end
