740d2c40f62b5e26422edcb5a17d3a010ab571d3
Byronlee <lbyronlee@gmail.com>
2015-05-09 16:23:18 +0800

    自定义导航用户信息接口的返回内容

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,4 @@ class UsersController < ApplicationController
     return render :text => '', layout: false if params['data'].blank? || params['data']['code'].to_i != 0
     render '_messages', layout: false
   end
-
-  def current
-    render json: current_user.to_json
-  end
 end
---
+++ app/views/users/current.jbuilder
@@ -0,0 +1,1 @@
+json.(current_user, :id, :email, :phone, :role, :authentication_token, :name, :bio, :tagline, :avatar_url, :sso_id, :favorites_count)
