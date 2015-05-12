0d0b0f2485a0d716068365b9135e197bfc29b48d
tian-xiaobo <peterwillcn@msn.com>
2015-05-12 17:30:44 +0800

    搜索改为本地服务

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -8,21 +8,23 @@ describe SearchController do
     end
 
     context 'all posts' do
+      before { create :post, :published, title: '标题1', published_at: 1.hour.ago }
       before { get :search, q: '' }
       it { should respond_with(:success) }
     end
 
     context 'with results' do
-      let!(:old_post) { create :post, :published, title: '标题1', published_at: 1.hour.ago }
-      let!(:new_post) { create :post, :published, title: '标题2', published_at: 1.minute.ago }
+      let!(:old_post) { create :post, :published, title: 'title 1', published_at: 1.hour.ago }
+      let!(:new_post) { create :post, :published, title: 'title 2', published_at: 1.minute.ago }
       before { Post.index.refresh }
-      before { get :search, q: '标题' }
+      before { get :search, q: 'title' }
       it do
         should respond_with(:success)
         expect(assigns[:posts].first).to eq(new_post)
       end
     end
     context 'not result' do
+      before { create :post, :published, title: 'oo-1xx', published_at: 1.hour.ago }
       before { get :search, q: 'oo-1xx' }
       it { should respond_with(:success) }
     end
