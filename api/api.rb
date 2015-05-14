cc691a1bfa8f8f6e272954a2bb1886a0f05360b0
Byronlee <lbyronlee@gmail.com>
2015-05-14 17:36:50 +0800

    去除gape log

--- api/api.rb
+++ api/api.rb
@@ -1,12 +1,7 @@
 require_relative 'mesh'
 module API
   class API < Grape::API
-
-    logger.formatter = GrapeLogging::Formatters::Default.new
-    use GrapeLogging::Middleware::RequestLogger, { logger: logger }
-
     mount API::V1::Base
     mount API::V2::Base
-
   end
 end
