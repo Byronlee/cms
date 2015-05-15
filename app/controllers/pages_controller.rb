a2543d70cdbe1f9e193f3edf4e9e3fb8af904dcd
Byronlee <lbyronlee@gmail.com>
2015-05-15 20:57:21 +0800

    给招聘页面读取文件添加缓存

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,13 +4,15 @@ class PagesController < ApplicationController
   end
 
   def hire
-    @jobs, @pages = Rails.cache.fetch(:page_hire, expires_in: 1.hour) do
+    @jobs, @pages = Rails.cache.fetch(:page_hire_, expires_in: 1.hour) do
+      jobs, pages = [], []
+      Rails.logger.info "reloading hire page text"
       Dir.glob("#{Rails.root}/public/hires/proceed/*").map do |file|
-        @jobs << (filename = File.basename(file, '.md').match(/\.(.+)/)[1].to_s)
+        jobs << (filename = File.basename(file, '.md').match(/\.(.+)/)[1].to_s)
         change_content = File.read(file)
-        @pages << [filename, GitHub::Markdown.render_gfm(change_content).html_safe]
+        pages << [filename, GitHub::Markdown.render_gfm(change_content).html_safe]
       end
-      [@job, @pages]
+      [jobs, pages]
     end
   end
 end
