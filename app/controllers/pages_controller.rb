5ed95a0b83c0a9a3ac4536a6e7f33a83e687112d
bevis <i@bevis.me>
2015-05-15 20:41:39 +0800

    招聘页面读取文件添加缓存

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,11 +4,13 @@ class PagesController < ApplicationController
   end
 
   def hire
-    @jobs, @pages = [], []
-    Dir.glob("#{Rails.root}/public/hires/proceed/*").map do |file|
-      @jobs << (filename = File.basename(file, '.md').match(/\.(.+)/)[1].to_s)
-      change_content = File.read(file)
-      @pages << [filename, GitHub::Markdown.render_gfm(change_content).html_safe]
+    @jobs, @pages = Rails.cache.fetch(:page_hire, expires_in: 1.hour) do
+      Dir.glob("#{Rails.root}/public/hires/proceed/*").map do |file|
+        @jobs << (filename = File.basename(file, '.md').match(/\.(.+)/)[1].to_s)
+        change_content = File.read(file)
+        @pages << [filename, GitHub::Markdown.render_gfm(change_content).html_safe]
+      end
+      [@job, @pages]
     end
   end
 end
