559f027767293d50047f28936edb0c48e466b632
chad <chad_lwm@hotmail.com>
2015-05-19 09:57:51 +0800

    支持html片段代码渲染

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,14 +2,22 @@ class WelcomeController < ApplicationController
   authorize_object :welcome
 
   def index
-    return cache_index if (params[:page].blank? || params[:page].to_i == 1) && (params[:d].blank? || params[:b_url_code].blank?)
+    return cache_index if (params[:page].blank? || params[:page].to_i == 1) && !api_request?
     info_flow = InfoFlow.find_by_name InfoFlow::DEFAULT_INFOFLOW
     page_direction = params[:d]
     boundary_post_url_code = params[:b_url_code]
     @posts_with_ads, @total_count, @prev_page, @next_page, @min_url_code, @max_url_code = info_flow.posts_with_ads(params[:page], page_direction, boundary_post_url_code)
 
     respond_to do |format|
-      format.html
+      format.html do
+        if api_request?
+          render 'welcome/_info_flows', locals: {
+            :posts_with_ads => @posts_with_ads, 
+            :prev_page => @prev_page, 
+            :next_page => @next_page
+          }, layout: false 
+        end
+      end
       format.json do 
         render json: { 
           :total_count => @total_count,
@@ -48,5 +56,11 @@ class WelcomeController < ApplicationController
   def site_map
     info_flow = InfoFlow.find_by_name InfoFlow::DEFAULT_INFOFLOW
     @posts = info_flow.posts
+  end
+
+  private
+
+  def api_request?
+    params[:d].present? && params[:b_url_code].present?
   end
 end
