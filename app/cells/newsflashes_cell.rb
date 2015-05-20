a2077057f02b4c8c6c29f0e839d1f3d17660a6ea
Byronlee <lbyronlee@gmail.com>
2015-05-20 10:38:27 +0800

    product ntoe数据真实化

--- app/cells/newsflashes_cell.rb
+++ app/cells/newsflashes_cell.rb
@@ -2,7 +2,7 @@ class NewsflashesCell < Cell::Rails
   helper ApplicationHelper
 
   def index
-    @newsflashes = Newsflash.order(:toped_at, created_at: :asc).limit 5
+    @newsflashes = Newsflash.tagged_with('_newsflash').order(:toped_at, created_at: :asc).limit 5
     render
   end
 
--- app/cells/product_note_cell.rb
+++ app/cells/product_note_cell.rb
@@ -2,5 +2,7 @@ class ProductNoteCell < Cell::Rails
   helper ApplicationHelper
 
   def index
+    @pdnotes = Newsflash.tagged_with('_pdnote').order(created_at: :asc).limit 5
+    render
   end
 end
