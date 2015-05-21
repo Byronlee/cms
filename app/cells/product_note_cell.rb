38a347d21e9cd4dbc0c38cd6fde38e5f72762cf1
Byronlee <lbyronlee@gmail.com>
2015-05-21 15:32:03 +0800

    快讯读取时间排序（product note）

--- app/cells/product_note_cell.rb
+++ app/cells/product_note_cell.rb
@@ -2,7 +2,7 @@ class ProductNoteCell < Cell::Rails
   helper ApplicationHelper
 
   def index
-    @pdnotes = Newsflash.tagged_with('_pdnote').order(created_at: :asc).limit 5
+    @pdnotes = Newsflash.tagged_with('_pdnote').order(created_at: :desc).limit 5
     render
   end
 end
