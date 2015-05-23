d04ed5c6f4776502fede8757b50a3c278d17fbf1
chad <chad_lwm@hotmail.com>
2015-05-23 10:54:38 +0800

    修改product node 和 newsfalsh默认加载条数

--- app/cells/newsflashes_cell.rb
+++ app/cells/newsflashes_cell.rb
@@ -2,7 +2,7 @@ class NewsflashesCell < Cell::Rails
   helper ApplicationHelper
 
   def index
-    @newsflashes = Newsflash.tagged_with('_newsflash').order(:toped_at, created_at: :desc).limit 5
+    @newsflashes = Newsflash.tagged_with('_newsflash').order(:toped_at, created_at: :desc).limit 20
     render
   end
 
