16a86b40f3e198d5051e3ae72583f95f90b9b22f
chad <chad_lwm@hotmail.com>
2015-05-26 14:24:12 +0800

    Tag列表页样式完成

--- config/routes.rb
+++ config/routes.rb
@@ -140,7 +140,7 @@ Rails.application.routes.draw do
   match '/feed/bdnews_feed_d9rIUTwdPm' => 'posts#feed_bdnews', via: :get, defaults: { format: :rss }
   match '/feed(/:params).:format' => redirect('/feed'), via: :get
   match '/feed(/:params)' => 'posts#feed', via: :get, defaults: { format: :rss }
-  match '/tag/:tag', :controller => 'tags', :action => 'show', via: :get
+  match '/tag/:tag', :controller => 'tags', :action => 'show', via: :get, as: :tag
   match '/clipped/:year/:month/:day', :controller => 'newsflashes', :action => 'index', via: :get, as: :newsflashes_of_day
   match '/clipped/:id', :controller => 'newsflashes', :action => 'show', via: :get, as: :newsflash_show
   match '/changelog', :controller => 'welcome', :action => 'changes', via: :get, as: :changes
