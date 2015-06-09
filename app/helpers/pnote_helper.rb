5ba7595bb738b6aff5ca69e318cabe6e00ff4932
chad <chad_lwm@hotmail.com>
2015-06-09 11:22:04 +0800

    fix:product note 格式显示

--- app/helpers/pnote_helper.rb
+++ app/helpers/pnote_helper.rb
@@ -1,8 +1,13 @@
 module PnoteHelper
-  def display_pnote_summary(pnote)
-  	return nil if pnote.news_summaries.blank?
-  	summary = pnote.news_summaries.strip.split(/\r\n/).delete_if(&:blank?)
-  	return nil if summary[0].blank?
-  	summary[0].gsub(/^-/, '' )
+  def display_pnote_summary(pnote, position)
+    return nil if pnote.news_summaries.blank?
+    summary = pnote.news_summaries.strip.split(/\r\n/).delete_if(&:blank?)
+    if position == :first
+      result = summary[0]
+    elsif position == :last
+      result = summary.last
+    end
+    return nil if result.blank?
+    result.gsub(/^-/, '' )
   end
 end
