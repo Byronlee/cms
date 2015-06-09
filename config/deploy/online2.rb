071fb7275212e137dcdfc3e66468a4f2a6c994c3
tian-xiaobo <peterwillcn@msn.com>
2015-06-09 18:46:25 +0800

    新版发布

--- config/deploy/online2.rb
+++ config/deploy/online2.rb
@@ -7,8 +7,9 @@ set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all
 
 server 'www-data@119.254.100.96', roles: %w[web app db], port: 52221, primary: true #, sidekiq: true, whenever: true
+server 'www-data@119.254.100.96', roles: %w{web app}, port: 52222
 server 'www-data@119.254.100.96', roles: %w{web app}, port: 52227
 
-namespace :deploy do
-  after "deploy:finished", "deploy:online_lbp"
-end
+#namespace :deploy do
+#  after "deploy:finished", "deploy:online_lbp"
+#end
