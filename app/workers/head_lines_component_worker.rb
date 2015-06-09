17b375ea478ae5af01583303ecdddca30dc9cff4
chad <chad_lwm@hotmail.com>
2015-06-09 18:38:04 +0800

    头条将一个广告换位头条内容

--- app/workers/head_lines_component_worker.rb
+++ app/workers/head_lines_component_worker.rb
@@ -3,7 +3,7 @@ class HeadLinesComponentWorker < BaseWorker
   sidekiq_options :queue => :third_party_headline, :backtrace => true
 
   def perform
-    head_lines = HeadLine.published.where.not(title: [nil, ""]).order(updated_at: :desc).limit(3)
+    head_lines = HeadLine.published.where.not(title: [nil, ""]).order(updated_at: :desc).limit(4)
     head_lines = head_lines.sort { |a, b| b.order_num.to_i <=> a.order_num.to_i }
     Redis::HashKey.new('head_lines')['list'] = head_lines.to_json()
   end
