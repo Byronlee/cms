bf7fedd10f474d91d90074b6ddf929b8199144f2
tian-xiaobo <peterwillcn@msn.com>
2015-06-10 13:01:46 +0800

    接口放出用户信息

--- api/v2/entities/user.rb
+++ api/v2/entities/user.rb
@@ -5,6 +5,9 @@ module V2
       expose :display_name, as: :name   , documentation: '名字'
       expose :avatar, as: :avatar_url , documentation: ''
       expose :sso_id        , documentation: ''
+      expose :email      , documentation: '邮箱'
+      expose :phone      , documentation: '电话'
+      expose :role        , documentation: ''
     end
   end
 end
