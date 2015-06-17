e3bf25af4e9c75bd4224bceee2121422867f2396
tian-xiaobo <peterwillcn@msn.com>
2015-06-17 12:14:03 +0800

    处理搜索特殊字符

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,11 @@
+require 'uri'
 class SearchController < ApplicationController
   def search
-    if params[:q].blank? 
+    params[:q] = URI.unescape(params[:q]).gsub('/','')
+    if params[:q].blank?
       @message = '搜索关键词不能为空'
       @posts = Post.none
-    elsif params[:q].gsub!('/','').length > Settings.elasticsearch.query.max_length
+    elsif params[:q].length > Settings.elasticsearch.query.max_length
       @message = '搜索关键词过长'
       @posts = Post.none
     else
