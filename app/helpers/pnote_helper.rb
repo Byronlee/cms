1e5227347faebdc8a0198ca59973918106819985
chad <chad_lwm@hotmail.com>
2015-07-24 13:18:52 +0800

    product note 显示，兼容以前

--- app/helpers/pnote_helper.rb
+++ app/helpers/pnote_helper.rb
@@ -1,13 +1,25 @@
 module PnoteHelper
+  def display_pnote_what(pnote)
+    pnote.what || pnote.description_text
+  end
+
   def display_pnote_summary(pnote, position)
-    return nil if pnote.news_summaries.blank?
-    summary = pnote.news_summaries.strip.split(/\r\n/).delete_if(&:blank?)
-    if position == :first
-      result = summary[0]
-    elsif position == :last
-      result = summary.last
+    if pnote.news_summaries.blank?
+      case position
+      when :first
+        pnote.how
+      when :last
+        pnote.think_it_twice
+      end
+    else
+      summary = pnote.news_summaries.strip.split(/\r\n/).delete_if(&:blank?)
+      if position == :first
+        result = summary[0]
+      elsif position == :last
+        result = summary.last
+      end
+      return nil if result.blank?
+      result.gsub(/^-/, '' )
     end
-    return nil if result.blank?
-    result.gsub(/^-/, '' )
   end
 end
