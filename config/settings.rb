6f73dcaee6ffc420a4d569e40d289e5f9dbf68af
Byronlee <lbyronlee@gmail.com>
2015-07-28 15:28:48 +0800

    添加专栏作者角色

--- config/settings.rb
+++ config/settings.rb
@@ -58,8 +58,8 @@ settings.next = {
   collection_api: 'https://next.36kr.com/api/v1/posts/collections'
 }
 
-# 读者, 运营, 作者, 编辑, 管理员, 投稿
-settings.roles =  [:reader, :operator, :writer, :editor, :admin, :contributor]
+# 读者, 运营, 作者, 编辑, 管理员, 投稿, 专栏作者
+settings.roles =  [:reader, :operator, :writer, :editor, :admin, :contributor, :column_writer]
 settings.user_search_support = [['角色', 'role'], ['ID', 'id'], ['姓名', 'name'], ['电话', 'phone'], ['邮件', 'email'], ['sso_id', 'sso_id']]
 settings.editable_roles =  [:operator, :writer, :editor, :admin]
 
