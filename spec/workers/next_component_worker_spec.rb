c2b7df0b363ea548635aef0c9a25a7897f932a6a
Byronlee <lbyronlee@gmail.com>
2015-08-05 11:28:01 +0800

    去掉没有用得next组件

--- spec/workers/next_component_worker_spec.rb
+++ spec/workers/next_component_worker_spec.rb
@@ -1,16 +1,16 @@
 require "spec_helper"
 
 describe NextComponentWorker, sidekiq: :fake do
-  it { should be_retryable true }
-  it { should be_retryable true }
-  it { should be_processed_in :third_party_next }
+  # it { should be_retryable true }
+  # it { should be_retryable true }
+  # it { should be_processed_in :third_party_next }
 
-  context ".perform" do
-    let(:next_redis_db) { Redis::HashKey.new('next') }
+  # context ".perform" do
+  #   let(:next_redis_db) { Redis::HashKey.new('next') }
 
-    it "enqueues access next collections job" do
-      subject.perform
-      expect { next_redis_db["collections"] }.to_not be_nil
-    end
-  end
+  #   it "enqueues access next collections job" do
+  #     subject.perform
+  #     expect { next_redis_db["collections"] }.to_not be_nil
+  #   end
+  # end
 end
