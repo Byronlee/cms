af5c1f2681801744fcd97c012cf2083db1959399
Byronlee <lbyronlee@gmail.com>
2015-08-06 16:10:57 +0800

    扩充词库

--- config/initializers/string.rb
+++ config/initializers/string.rb
@@ -7,7 +7,7 @@ class String
     self.gsub! /([\d\p{Han}：:]|\s|^)([a-zA-Z\d\-\_\.]+)([\d\p{Han},，。；]|\s|$)/u do
       key = "#$2".downcase
       if dicts.has_key?(key)
-        ["#$1",AutoCorrect::DICTS[key],"#$3"].join("")
+        ["#$1", dicts[key], "#$3"].join("")
       else
         "#$1#$2#$3"
       end
