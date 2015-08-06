1c5f25dbcdd5206923a1f2fbc82dbf9002f8a371
Byronlee <lbyronlee@gmail.com>
2015-08-06 14:59:36 +0800

    扩展拼写库

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -3,6 +3,7 @@ class Settings < Fume::Settable::Base
   ruby_provider Rails.root.join('config/settings.local.rb')
   ruby_provider Rails.root.join('config/settings.rb')
   ruby_provider Rails.root.join('config/pages.rb')
+  ruby_provider Rails.root.join('config/spell_dicts.rb')
 
 
   def self.method_missing(name, *args, &block)
