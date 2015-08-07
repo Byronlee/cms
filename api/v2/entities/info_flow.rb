e13223c39fe192282df9fea17a7aebd4fda713cb
tian-xiaobo <peterwillcn@msn.com>
2015-08-07 11:49:17 +0800

    信息流放出专栏

--- api/v2/entities/info_flow.rb
+++ api/v2/entities/info_flow.rb
@@ -3,6 +3,7 @@ module V2
     class InfoFlow < Grape::Entity
       expose :id,           documentation: 'not null, primary key'
       expose :name,         documentation: ''
+      expose :columns, using: Entities::Column, documentation: '专栏'
       format_with(:iso_timestamp) { |dt| dt.iso8601 if dt }
       with_options(format_with: :iso_timestamp) do
         expose :created_at, documentation: ''
