7119cfa913e2fed188e76e43a68d647a36be177e
tian-xiaobo <peterwillcn@msn.com>
2015-08-07 18:42:36 +0800

    key

--- api/v2/entities/post_detail.rb
+++ api/v2/entities/post_detail.rb
@@ -7,6 +7,7 @@ module V2
       expose :sanitize_content, as: :content , documentation: '内容'
       expose :favoriter_sso_ids
       expose :tag_list   , documentation: '标签'
+      expose :key        , documentation: 'writer使用'
       with_options(format_with: :iso_timestamp) do
         expose :created_at , documentation: ''
         expose :updated_at , documentation: ''
