ef171c61ec189faef0bc2cc4686f95f1518226c0
chad <chad_lwm@hotmail.com>
2015-08-20 17:30:46 +0800

    修改next获取协议

--- app/assets/javascripts/krx/config.js
+++ app/assets/javascripts/krx/config.js
@@ -1,6 +1,6 @@
 window.KR_CONFIG_OBJECT = {
     fetchFeed: 'https://rong.36kr.com/api/hostsite/fetchFeeds',
     trackClipPage: '/clipped/{id}/touch_view?authenticity_token=' + window._token,
-    fetchNext: 'http://next.36kr.com/posts/latest.json',
+    fetchNext: '//next.36kr.com/posts/latest.json',
     nextPageSize: 20
 };
