cfc9ac4bff9c4555294de0b4692c8ce4be75ee11
tian-xiaobo <peterwillcn@msn.com>
2015-08-20 18:53:14 +0800

    kr搜素回调

--- api/v2/controllers/callbacks.rb
+++ api/v2/controllers/callbacks.rb
@@ -11,6 +11,19 @@ module V2
           post.increase_mobile_views_count if post
         end
 
+        desc 'get time updated posts url_code'
+        params do
+          optional :time,  type: Integer, default: Time.now.to_i*100, desc: '时间'
+        end
+        post 'posts' do
+          if ((Date.today.end_of_day - Time.at(params[:time].to_i/100)) / (3600*24)) < 15
+            posts = Post.select(:url_code).where(:published_at => Time.at(params[:time].to_i/100)..Date.today.end_of_day)
+            posts.map(&:url_code)
+          else
+            { msg: '大哥，注意身体不要用力过猛哦.' }
+          end  
+        end
+
       end
 
     end
