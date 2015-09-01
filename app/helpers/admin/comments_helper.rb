478d3af66cd13b9a9640836024f55fade201bc99
chad <chad_lwm@hotmail.com>
2015-09-01 22:05:50 +0800

    用户只能查看已发布的评论或自己的评论

--- app/helpers/admin/comments_helper.rb
+++ app/helpers/admin/comments_helper.rb
@@ -2,11 +2,21 @@ module Admin::CommentsHelper
   def display_comment_state(state)
     case state
     when 'reviewing'
-      raw "<span title='审查中' class='badge badge-warning'>#{state}</i></span>"
+      raw "<span title='审查中' class='badge badge-warning'>审查中</i></span>"
     when 'published'
-      raw "<span title='已发布' class='badge badge-success'>#{state}</i></span>"
-    else
-      raw "<span title='已屏蔽' class='badge badge-fatal'>#{state}</i></span>"
+      raw "<span title='已发布' class='badge badge-success'>已发布</i></span>"
+    when 'rejected'
+      raw "<span title='已屏蔽' class='badge badge-fatal'>已屏蔽</i></span>"
+    end
+  end
+
+
+  def display_comment_state_with_tip(state)
+    case state
+    when 'reviewing'
+      raw "<span title='审查中, 该评论仅本人可见' class='badge badge-warning'>审查中</i></span>"
+    when 'rejected'
+      raw "<span title='已屏蔽，该评论仅本人可见' class='badge badge-fatal'>已屏蔽</i></span>"
     end
   end
 
