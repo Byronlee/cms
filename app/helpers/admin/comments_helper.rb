dc45db085070acbb566109af04226a19d89a9770
chad <chad_lwm@hotmail.com>
2015-09-06 16:04:46 +0800

    fix:用户评论被屏蔽后，查看已屏蔽的小标，提示信息出现错别字

--- app/helpers/admin/comments_helper.rb
+++ app/helpers/admin/comments_helper.rb
@@ -1,22 +1,33 @@
 module Admin::CommentsHelper
   def display_comment_state(state)
-    case state
-    when 'reviewing'
-      raw "<span title='审查中' class='badge badge-warning'>审查中</i></span>"
-    when 'published'
-      raw "<span title='已发布' class='badge badge-success'>已发布</i></span>"
-    when 'rejected'
-      raw "<span title='已屏蔽' class='badge badge-fatal'>已屏蔽</i></span>"
-    end
+    raw "<span title='#{Comment::STATE_OPTIONS[state.to_sym]}' class='badge #{comment_state_style(state)}'>#{Comment::STATE_OPTIONS[state.to_sym]}</i></span>"
   end
 
 
   def display_comment_state_with_tip(state)
+    return if state == 'published'
+    raw "<span title='#{comment_state_tip(state)}' class='badge #{comment_state_style(state)}'>#{Comment::STATE_OPTIONS[state.to_sym]}</i></span>"
+  end
+
+  private
+  def comment_state_style(state)
     case state
     when 'reviewing'
-      raw "<span title='该评论正在审查中, 仅对本人可见' class='badge badge-warning'>审查中</i></span>"
+      "badge-warning"
+    when 'published'
+      "badge-success"
     when 'rejected'
-      raw "<span title='改评论已经屏蔽，仅对本人可见' class='badge badge-fatal'>已屏蔽</i></span>"
+      "badge-fatal"
+    end
+  end
+
+
+  def comment_state_tip(state)
+    case state
+    when 'reviewing'
+      "该评论正在审查中, 仅对本人可见"
+    when 'rejected'
+      "该评论已经屏蔽，仅对本人可见"
     end
   end
 
