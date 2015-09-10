c68615107ea5efce8fa0b0531cacb08db9ed6e98
chad <chad_lwm@hotmail.com>
2015-09-10 19:15:39 +0800

    fix:用户未登录获取评论报错问题

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -44,7 +44,7 @@ class Comment < ActiveRecord::Base
     includes(:commentable, user: [:krypton_authentication]).order('char_length(content) desc')
   }
   scope :excellent, -> { includes(:post).where(posts: { state: :published }, is_excellent: true) }
-  scope :valid_comments, -> (user) { where("state = 'published' or user_id = ? ", user.id) if user }
+  scope :valid_comments, -> (user) { user ? where("state = 'published' or user_id = ? ", user.id) : where("state = 'published'") }
 
   aasm do
     state :reviewing, :initial => true
