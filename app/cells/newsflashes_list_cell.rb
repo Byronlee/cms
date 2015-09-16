f8be2e47045deec0345d08b643777d388808ced3
Byronlee <lbyronlee@gmail.com>
2015-09-16 19:48:14 +0800

    支持Tag的筛选[更加当前一周，选出最热tag]

--- app/cells/newsflashes_list_cell.rb
+++ app/cells/newsflashes_list_cell.rb
@@ -6,11 +6,13 @@ class NewsflashesListCell < Cell::Rails
     render
   end
 
-  def tags
+  def tags(arg)
+    @tags = Newsflash.pnews.where(created_at: 1.week.ago..DateTime.now).tag_counts_on(:tags).order(taggings_count: :desc).limit(arg[:num])
     render
   end
 
   def hot
+    @newsflashes = Newsflash.pnews.where(created_at: 1.week.ago..DateTime.now).order(created_at: :desc).limit 10
     render
   end
 end
