f861f10a9dca291e3ba2997680d798969a42ca3d
chad <chad_lwm@hotmail.com>
2015-09-17 11:09:45 +0800

    文章底部寻求报道新入口内容移到配置文件

--- config/settings.rb
+++ config/settings.rb
@@ -134,3 +134,5 @@ settings.cache={
 settings.subsite = 'http://space.36kr.com'
 
 settings.user_info_update_url = '//uc.36kr.com/#/uc/account/basic'
+
+settings.report_tip = '<p>“看完这篇还不够？如果你也在创业，并且希望自己的项目被报道，请<a href="http://chuang.36kr.com/report#/report/index" target="_blank">戳这里</a>告诉我们！”</p>'
