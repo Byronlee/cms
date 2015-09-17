a2d7a13c51888eebc7562cc86f5c808a5069001c
chad <chad_lwm@hotmail.com>
2015-09-17 17:00:15 +0800

    为tag添加链接

--- app/cells/newsflashes_list_cell.rb
+++ app/cells/newsflashes_list_cell.rb
@@ -7,12 +7,13 @@ class NewsflashesListCell < Cell::Rails
   end
 
   def tags(arg)
-    @tags = Newsflash.pnews.where(created_at: 1.week.ago..DateTime.now).tag_counts_on(:tags).order(taggings_count: :desc).limit(arg[:num])
+    @tags = Newsflash.newsflashes.where(created_at: 1.week.ago..DateTime.now).tag_counts_on(:tags).order(taggings_count: :desc).limit(arg[:num])
+    @tags = @tags.to_a.delete_if{|tag| tag.name == '_newsflash'}
     render
   end
 
   def hot
-    @newsflashes = Newsflash.pnews.where(created_at: 1.week.ago..DateTime.now).order(created_at: :desc).limit 10
+    @newsflashes = Newsflash.newsflashes.where(created_at: 1.week.ago..DateTime.now).order(created_at: :desc).limit 10
     render
   end
 end
