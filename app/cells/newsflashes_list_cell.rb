99f4a302cbd5c9d9b4c708ab82aae07114582fbc
chad <chad_lwm@hotmail.com>
2015-09-24 16:39:37 +0800

    添加快讯热点缓存

--- app/cells/newsflashes_list_cell.rb
+++ app/cells/newsflashes_list_cell.rb
@@ -8,6 +8,10 @@ class NewsflashesListCell < Cell::Rails
     render
   end
 
+  cache :tags, :expires_in => 1.minutes do |arg|
+    [:newsflashes_list, :tags, arg[:from]]
+  end
+
   def tags(arg)
     @tags = Newsflash.newsflashes.where(created_at: 1.week.ago..DateTime.now).tag_counts_on(:tags).order(taggings_count: :desc).limit(arg[:num])
     @tags = @tags.to_a.delete_if{|tag| tag.name == '_newsflash'}
