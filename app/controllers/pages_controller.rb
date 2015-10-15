f20844a44fc577e6c9ebadd0f8a44b5e3f7bffc4
chad <chad_lwm@hotmail.com>
2015-10-15 20:09:21 +0800

    修正招聘文件排序

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,15 +4,25 @@ class PagesController < ApplicationController
   end
 
   def hire
+    Rails.cache.delete_matched(:v3_page_hire_) if params[:force_flush] == 'true'
     @jobs, @pages = Rails.cache.fetch(:v3_page_hire_, expires_in: 1.hour) do
       jobs, pages = [], []
       Rails.logger.info "reloading hire page text"
-      Dir.glob("#{Rails.root}/public/hires/proceed/*").map do |file|
+      Dir.glob("#{Rails.root}/public/hires/proceed/*")
+      .sort do |a ,b|
+        hire_file_num(a) <=> hire_file_num(b)
+      end
+      .map do |file|
         jobs << (filename = File.basename(file, '.md').match(/\.(.+)/)[1].to_s)
         change_content = File.read(file)
         pages << [filename, GitHub::Markdown.render_gfm(change_content).html_safe]
       end
-      [jobs, pages]
+    [jobs, pages]
     end
+  end
+
+  private
+  def hire_file_num(path)
+    path.split('/').last.split('.').first.to_i
   end
 end
